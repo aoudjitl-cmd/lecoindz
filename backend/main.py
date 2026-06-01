a80f48d83c20591802c2a7181950f550add0c24a
aoudjitl-cmd <aoudjitl@gmail.com>
2026-06-01 16:54:20 +0200

    Fix CORS pour Vercel

--- backend/main.py
+++ backend/main.py
@@ -15,6 +15,8 @@ app.add_middleware(
         "http://127.0.0.1:3000",
         "http://localhost:8000",
         "http://127.0.0.1:8000",
+        "https://rayahdz.vercel.app",
+        "https://*.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
