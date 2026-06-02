644f896ab78cbc81d3a11bb806748e6bcf89bf62
aoudjitl-cmd <aoudjitl@gmail.com>
2026-06-02 10:32:21 +0200

    Modification de database py 2

--- backend/src/config/database.py
+++ backend/src/config/database.py
@@ -2,12 +2,12 @@ from src.config.settings import settings
 import oracledb
 
 def get_connection():
+    dsn = f"(description=(address=(protocol=tcps)(host={settings.ORACLE_HOST})(port={settings.ORACLE_PORT}))(connect_data=(service_name={settings.ORACLE_SERVICE})))"
+
     connection = oracledb.connect(
         user=settings.ORACLE_USER,
         password=settings.ORACLE_PASSWORD,
-        host=settings.ORACLE_HOST,
-        port=settings.ORACLE_PORT,
-        service_name=settings.ORACLE_SERVICE,
-        protocol="tcps"
+        dsn=dsn
     )
+
     return connection
