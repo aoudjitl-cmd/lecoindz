7d4f5a73173eb71edae42b8d4cf8b164efd1f4a2
aoudjitl-cmd <aoudjitl@gmail.com>
2026-06-10 12:50:46 +0200

    verification mail

--- backend/src/routes/subscriptions.py
+++ backend/src/routes/subscriptions.py
@@ -74,7 +74,7 @@ def create_checkout(current_user=Depends(get_current_user)):
             }],
             mode="subscription",
             subscription_data={
-                "trial_period_days": 30
+                "trial_period_days": 0
             },
             success_url="https://www.rayahdz.com/subscription-success.html?session_id={CHECKOUT_SESSION_ID}",
             cancel_url="https://www.rayahdz.com/subscription.html",
